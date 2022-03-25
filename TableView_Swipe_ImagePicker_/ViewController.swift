fef966f81cc6a9f06bb8839f45207e258f35ab6b
Yasser Hajlaoui <yasser159@gmail.com>
2022-03-24 23:53:39 -0700

    Initial Commit

---
+++ TableView_Swipe_ImagePicker_/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableView_Swipe_ImagePicker_
+//
+//  Created by Yasser Hajlaoui on 3/24/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
